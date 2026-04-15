aed882973e797297ddafac9de52fe55c535b4f9e
Mateusz <mateusz@smuga.pl>
2026-04-15 09:44:36 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,27 @@ vim.g.maplocalleader = ' '
 
 vim.opt.relativenumber = true
 
-require("core.decorations")
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true 
+
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.termguicolors = true
+vim.opt.fillchars = vim.opt.fillchars + { eob = " " }
+
 require("core.keymaps")
 require("plugins")
